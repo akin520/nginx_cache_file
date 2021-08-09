dfbfd8dba47214a23828c8468e5128bd56cbded5
akin520 <badb0y520@gmail.com>
2021-08-09 14:00:44 +0800

    add path env

--- cache_file.lua
+++ cache_file.lua
@@ -1,3 +1,6 @@
+--- https://blog.csdn.net/cmy1987/article/details/108571199
+require "resty.core"
+
 local function write_file(file_name, content)
     if not file_name then
         return nil,"missing file_name"
@@ -23,6 +26,7 @@ local function read_file(file_name)
     file:close()
     return content, nil
 end
+
 function get_from_cache(key)
     local cache_ngx = ngx.shared.my_cache
     local value = cache_ngx:get(key)
@@ -41,14 +45,19 @@ function set_to_cache(key,value,exptime)
     return succ
 end
 
+
+
+
 local uri = ngx.var.uri;
 local uri_args = ngx.req.get_uri_args();
 local request_uri = ngx.var.request_uri;
 local md5 = ngx.md5
 local key = md5(request_uri)
+--- ngx.say(request_uri);
 
 --- https://blog.csdn.net/wtswjtu/article/details/38898945
-cache_file = string.gsub(request_uri,"/","_")
+local cache_file = string.gsub(request_uri,"/","_")
+local file_path = ngx.var.cachepath .. "/" .. ngx.var.host .. "/" .. cache_file
 
 local item_model = get_from_cache(key)
 --- local item_model = nil
@@ -71,23 +80,51 @@ if item_model == nil then
         }           
     )
     if not resp then
+	if ngx.var.oldcache == "on" then
+        local content,err = read_file(file_path)
+        if err == nil then
+           ngx.header["Content-type"] = "text/html; charset=utf-8"
+           ngx.header["X-Cache"] = "resp-old-cache"
+           ngx.say(content)
+           ngx.exit(ngx.OK)
+        else
+            delete_from_cache(key)
+            ngx.header["X-Cache"] = "resp error"
+            ngx.exit(ngx.HTTP_BAD_GATEWAY)
+        end
+        end
         ngx.header["X-Cache"] = "geturl timeout"
         ngx.exit(ngx.HTTP_BAD_GATEWAY)   --- https://www.cnblogs.com/tinywan/p/6538006.html
     end
     if resp.status ~= 200 then
-	ngx.exit(resp.status)
+        if ngx.var.oldcache == "on" and resp.status >= 500 then
+            local content,err = read_file(file_path)
+            if err == nil then
+                ngx.header["Content-type"] = "text/html; charset=utf-8"
+                ngx.header["X-Cache"] = "old-cache"
+                ngx.say(content)
+                ngx.exit(ngx.OK)
+            else
+                delete_from_cache(key)
+                ngx.header["X-Cache"] = "500 read file error"
+                ngx.exit(resp.status)
+            end
+	else
+	    ngx.exit(resp.status)
+        end
     end
     ngx.header["Content-type"] = "text/html; charset=utf-8"
     ngx.header["X-Cache"] = "url-cache"
     ngx.say(resp.body);
     set_to_cache(key,1,2*86400)
-    write_file("/xq_data/cache/"..ngx.var.host.."/" .. cache_file, resp.body)
+    write_file(file_path, resp.body)
     httpc:close();
 else
-    local content,err = read_file("/xq_data/cache/"..ngx.var.host.."/" .. cache_file)
+    local content,err = read_file(file_path)
+    local ttl1,err1 = ngx.shared.my_cache:ttl(key)
     if err == nil then
         ngx.header["Content-type"] = "text/html; charset=utf-8"
-        ngx.header["X-Cache"] = "file-cache"
+        ngx.header["X-Cache"] = "file-cache:" ..ttl1
         ngx.say(content)
         ngx.exit(ngx.OK)
     else
